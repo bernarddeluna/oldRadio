c5334691c25a5ec391a36e7721a3f80bc1da2306
Djalma Araújo <djalma.araujo@gmail.com>
2012-07-12 23:40:33 -0300

    Complete remake of Konami code. New structure, supporting multiple instances + init first key fail support and more

--- js/konami.js
+++ js/konami.js
@@ -1,19 +1,45 @@
 YUI.add('konami', function (Y) {
-    Y.Event.define('konami', {
-      on: function (node, subscription, notifier) {
-          var pressedKeys = [],
-              konamiKeys = "38,38,40,40,37,39,37,39,66,65";
+	Y.Event.define('konami', {
+		_konamiSequence: [38,38,40,40,37,39,37,39,66,65],
 
-          subscription._handle = Y.on('keydown', function(e) {
-            pressedKeys.push( e.keyCode );
-            if ( pressedKeys.toString().indexOf( konamiKeys ) >= 0 ){
-              notifier.fire(e);
-            }
-          });
-      },
+		on: function (node, sub, notifier) {
+			var instance = this;
 
-      detach: function (node, subscription, notifier) {
-        subscription._handle.detach();
-      }
-    });
-}, '0.0.2', { requires: ['event'] });
+			sub._firstLength = 0;
+			sub._index = 0;
+			sub._handle = Y.on('keydown', Y.rbind(instance._handleKeyDown, instance, sub, notifier));
+		},
+
+		_handleKeyDown: function(e, sub, notifier) {
+			var instance = this,
+				index = sub._index,
+				konamiKeys = this._konamiSequence,
+				keyCode = e.keyCode;
+
+			if (sub._firstLength === 0) {
+				while (konamiKeys[0] === konamiKeys[++sub._firstLength]);
+			}
+
+			if (keyCode === konamiKeys[index])  {
+				if (++index === konamiKeys.length) {
+					notifier.fire(e);
+					index = 0;
+				}
+			}
+			else {
+
+				if ((index === sub._firstLength) && (keyCode === konamiKeys[0])) {
+					return;
+				}
+
+				index = 0;
+			}
+
+			sub._index = index;
+		},
+
+		detach: function (node, sub, notifier) {
+			sub._handle.detach();
+		}
+	});
+}, '1.0', { requires: ['event-synthetic'] });
